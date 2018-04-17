92d6d563b40fd6c891acdd988a465b677e31345e
Captain <mads130f@edu.easj.dk>
2018-04-17 14:01:05 +0200

    Man kan nu tegneXD

--- src/RunApplication.java
+++ src/RunApplication.java
@@ -1,6 +1,8 @@
 import javafx.animation.TranslateTransition;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
@@ -9,9 +11,12 @@ import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
+
 public class RunApplication extends Application {
     //scene.snapshot();//Ændrer baggrunden?
 
+    //Komplet guide
+    //https://docs.oracle.com/javafx/2/canvas/jfxpub-canvas.htm
 
     @Override
     public void start(Stage primaryStage) {
@@ -25,14 +30,20 @@ public class RunApplication extends Application {
         primaryStage.setScene(scene);
         primaryStage.show();
 
+
+
         //Class der følger mus
         FollowMouse followMouse = new FollowMouse();
         //Class der bestemmer hvilken type man vil tegne med.
         StylusType stylusType = new StylusType(1,Color.RED,10);
         //SelectionWindow
-        SelectionWindow selectionWindow = new SelectionWindow();
+        /*SelectionWindow selectionWindow = new SelectionWindow();
         selectionWindow.runSelectionWindow();
+*/
+        Canvas canvas = new Canvas(600,600);
+        root.getChildren().add(canvas);
 
+        GraphicsContext gc = canvas.getGraphicsContext2D();
 
 
 
@@ -41,15 +52,15 @@ public class RunApplication extends Application {
 
 
         scene.setOnMouseDragged(event -> {
-            if(event.isPrimaryButtonDown()){
-                //event.getSceneX()'erne er mouse pos
-                //Ændre den sidste para for at skrift "mousestyle"
-                followMouse.followTheDragMouse(event.getSceneX(),event.getSceneY(),stylusType.getTheNode());
-            }
-
+            gc.setFill(Color.RED);
+            gc.setStroke(Color.BLACK);
+            gc.setLineWidth(5);
+            //gc.strokeOval(event.getSceneX(),event.getSceneY(),1,1);
+            gc.strokeLine(event.getSceneX(),event.getSceneY(),event.getSceneX(),event.getSceneY());
         });
 
-        root.getChildren().add(stylusType.getTheNode());//Her tilføjer jeg node.
+        //Her tilføjer man den røde dot
+        //root.getChildren().add(stylusType.getTheNode());//Her tilføjer jeg node.
 
 
 
