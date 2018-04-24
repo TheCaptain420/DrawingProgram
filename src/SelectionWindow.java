29baeb8987067dec0a87e59040ee29e9977069bf
Captain <mads130f@edu.easj.dk>
2018-04-24 14:00:39 +0200

    xd

--- src/SelectionWindow.java
+++ src/SelectionWindow.java
@@ -1,13 +1,18 @@
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.Slider;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
 
 public class SelectionWindow  {
     private double sliderSize=0;
@@ -24,9 +29,11 @@ public class SelectionWindow  {
         grid.setHgap(5);
 
 
-        //Buttons
+        //TEstButton1
         Button hello = new Button("hello");
 
+
+        //testButton2
         Button test = new Button("Test");
 
         //Slider
@@ -37,7 +44,7 @@ public class SelectionWindow  {
         //kommer en getter længere nede
 
         //Slider button
-        Button sliderButton = new Button("ChangeSize");
+        Button sliderButton = new Button("Add");
         sliderButton.setOnAction(new EventHandler<ActionEvent>() {
             @Override
             public void handle(ActionEvent event) {
@@ -45,13 +52,23 @@ public class SelectionWindow  {
             }
         });
 
+        //StylusSelecter
+        ObservableList<String> stylustyper =
+                FXCollections.observableArrayList(
+                        //Skriv typer stylus herunder
+                        "Option 1",
+                        "Option 2",
+                        "Option 3"
+                );;
+        ComboBox stylusSelectionBox  = new ComboBox(stylustyper);
+        stylusSelectionBox.setValue("Vælg Stylus");
+        stylusSelectionBox.getValue();
 
-
-        grid.add(hello,1,1);
-        grid.add(test,1,2);
-        grid.add(slider,1,3);
-        grid.add(sliderButton,2,3);
-
+        grid.add(stylusSelectionBox,1,1);
+        grid.add(slider,1,2);
+        grid.add(sliderButton,2,2);
+        grid.add(hello,1,3);
+        grid.add(test,1,4);
 
 
         secondStage.setScene(new Scene(grid,200,200));
@@ -59,6 +76,13 @@ public class SelectionWindow  {
         secondStage.setY(0);
         secondStage.show();
 
+
+        hello.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent event) {
+                System.out.println(stylusSelectionBox.getValue());
+            }
+        });
 
 
         whatTheButtonsDo.doStuff(test);
